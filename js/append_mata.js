d661df04be46d838d8fd0ca248ffd59c6fffd986
thelustosa <lustozera@icloud.com>
2026-07-04 22:48:15 -0300

    novos controles

--- js/append_mata.js
+++
@@ -1,8 +0,0 @@
-window.i18nTranslations['PT'].mata_mata = 'MATA-MATA';
-window.i18nTranslations['EN'].mata_mata = 'KNOCKOUT';
-window.i18nTranslations['ES'].mata_mata = 'ELIMINATORIAS';
-window.i18nTranslations['FR'].mata_mata = 'ÉLIMINATION DIRECTE';
-window.i18nTranslations['PL'].mata_mata = 'FAZA PUCHAROWA';
-window.i18nTranslations['IT'].mata_mata = 'FASE A ELIMINAZIONE';
-if(window.i18nTranslations['JA']) window.i18nTranslations['JA'].mata_mata = 'ノックアウト';
-if(window.i18nTranslations['RU']) window.i18nTranslations['RU'].mata_mata = 'ПЛЕЙ-ОФФ';
